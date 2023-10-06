91430c9c5168603da97b3d7edc35321072b80e5b
Asif <shaikh.akhtar@integrumsolutions.in>
2023-10-06 19:24:28 +0530

    updated tar file

--- projects/hrsuite-shared-lib/src/lib/hrsuite-shared-lib.module.ts
+++ projects/hrsuite-shared-lib/src/lib/hrsuite-shared-lib.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { HRSuiteSharedLibComponent } from './hrsuite-shared-lib.component';
+import { StoreModule } from '@ngrx/store';
+import { categoryReducer, moviesReducer } from '../public-api';
 
 
 
@@ -8,6 +10,10 @@ import { HRSuiteSharedLibComponent } from './hrsuite-shared-lib.component';
     HRSuiteSharedLibComponent
   ],
   imports: [
+    StoreModule.forRoot(
+      { movies: moviesReducer, categories: categoryReducer },
+      {}
+    )
   ],
   exports: [
     HRSuiteSharedLibComponent
